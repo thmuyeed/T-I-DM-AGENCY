536df45665af36233e279abcb94dbd15295a80d6
Tasnemul Hasan Muyeed <thmuyeed29@gmail.com>
2022-11-09 00:19:49 +0600

    done

--- js/script.js
+++ js/script.js
@@ -324,21 +324,8 @@ var typed = new Typed('.type', {
     loop:true
   
 });
+// skill
 
-// team skill
-$(document).ready(function(){
-    $('.progress-value > span').each(function(){
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        },{
-            duration: 1500,
-            easing: 'swing',
-            step: function (now){
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-});
 
 // protect copy
 
